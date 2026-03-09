22f98ccaba444c6bfa8ed08d80bf77a872d0b0e6
Rahmat Hidayat <rahmat7rht@gmail.com>
2026-03-10 06:05:59 +0700

    add MergeSort class and mergeSort method

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
+class MergeSort {
+    public:
+        void mergeSort(int *array, int size) {
+            if (size <= 1) return;
+
+            int middle = size / 2;
+            int remain = size - (size * 2); //a remaining element if the size is odd
+
+            if (size > 2) {
+                mergeSort(array, middle); //left recursion
+                mergeSort(array + middle, middle + remain); //right recurtion
+                merge(array, size, middle);
+            }
+        }
+    private:
+        void merge(int *array, int size, int middle) {
+
+        }
+
+};
+
 int main() {
     
 }
