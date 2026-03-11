1e0f44f2b174afa8369d94fa4e85fa4f9e83bb38
Rahmat Hidayat <rahmat7rht@gmail.com>
2026-03-11 20:53:14 +0700

    make the code of bubble sort

---
+++ bubblesort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+class BubbleSort {
+    public:
+        void bubbleSort(int *array, size_t size) {
+            int n = 0;
+            int length = size;
+            while (length > 0) {
+                if (array[n] > array[n + 1]) {
+                    swap(array[n], array[n + 1]);
+                }
+                
+                if (!(++n < length)) {
+                    n = 0;
+                    length--;
+                }
+            }
+        }
+
+        void swap(int &a, int &b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+};
+
+void print(int *array, size_t size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << array[i] << ((i + 1) < size? ", ":"\n");
+    }
+}
+
+int main() {
+    int size = 8;
+    int array[size] = {5, 3, 99, 32, 44, 2, 13, 11};
+
+    print(array, size);
+
+    BubbleSort bubble;
+
+    bubble.bubbleSort(array, size);
+    print(array, size);
+}
