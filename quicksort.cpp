c4873a0b5940d47d63f9a43027e8243704c1374b
Rahmat Hidayat <rahmat7rht@gmail.com>
2026-03-14 23:58:01 +0700

    implement median of three for pivot

--- quicksort.cpp
+++ quicksort.cpp
@@ -8,8 +8,26 @@ class QuickSort {
 
     private:
         int partition(int *array, size_t size) {
+            //if the size is more than 2, implement median-of-three for pivot
+            if (size > 2) {
+                int mid = size / 2;
+                if (array[mid] < array[0]) {
+                    swap(array[mid], array[0]);
+                }
+
+                if (array[size - 1] < array[0]) {
+                    swap(array[size - 1], array[0]);
+                }
+
+                if (array[size - 1] < array[mid]) {
+                    swap(array[size - 1], array[mid]);
+                }
+
+                swap(array[mid], array[size - 1]); //with the median is sorted, then swap the middle with high, so that the pivot is picked the middle
+            }
+
             int *pivot = &array[size - 1]; //pick the lass element as pivot
-            int low = 0; 
+            size_t low = 0; 
 
             for (size_t i = 0; i < size - 1; i ++) {
                 if (array[i] < *pivot) {
@@ -27,7 +45,7 @@ class QuickSort {
         void quickSort(int *array, size_t size) {
             if (size <= 1) return; //break the recursion if the size is less than two
 
-            int pivot_index = partition(array, size); //get the pivot last index
+            size_t pivot_index = partition(array, size); //get the pivot last index
             size_t right_partition_size = size - pivot_index - 1; //calculate the size of the last partition after the pivot index
 
             quickSort(array, pivot_index); //make a new recursion as the size is upto pivot_index
@@ -42,13 +60,13 @@ class QuickSort {
 };
 
 void print(int *array, size_t size) {
-    for (int i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; i++) {
         std::cout << array[i] << ((i + 1) < size? ", ":"\n");
     }
 }
 
 int main() {
-    const int size = 9;
+    const size_t size = 9;
     int list[size] = {65, 33, 98, 7, 28, 66, 89, 9, 22};
     
     std::cout << "Unsorted List:\n";
